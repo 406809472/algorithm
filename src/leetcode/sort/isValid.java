c2a09131c7afb4de9be300ce4d106f3ce67ce1ad
ljl <ljl@qq.com>
2022-08-16 15:24:17 +0800

    isValid

--- src/leetcode/sort/isValid.java
+++ src/leetcode/sort/isValid.java
@@ -1,22 +1,26 @@
 package leetcode.sort;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
+import java.util.TreeSet;
 
-/**有效的括号
+/**
+ * 有效的括号
  * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
  * 有效字符串需满足：
- *
+ * <p>
  * 左括号必须用相同类型的右括号闭合。
  * 左括号必须以正确的顺序闭合
  */
 public class isValid {
-    public boolean solution(String s){
-        Stack<Character> stack=new Stack<>();
-        for(int i=0;i<s.length();i++){
-            char c=s.charAt(i);
-            if(c=='('||c=='['||c=='{'){stack.push(c);}
-            else if(stack.isEmpty()||c==')'&&stack.pop()!='('||c==']'&&stack.pop()!='['||c=='}'&&stack.pop()!='{'){
+    public boolean solution(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else if (stack.isEmpty() || c == ')' && stack.pop() != '(' || c == ']' && stack.pop() != '[' || c == '}' && stack.pop() != '{') {
                 return false;
             }
         }
@@ -24,4 +28,22 @@ public class isValid {
 
     }
 
+    /**
+     * 删除有序数组中的重复项
+     *
+     * @param nums
+     * @return
+     */
+    public static int removeDuplicates(int[] nums) {
+        TreeSet treeSet = new TreeSet();
+        for (int i = 0; i < nums.length; i++) {
+            treeSet.add(nums[i]);
+        }
+        Object[] array = treeSet.toArray();
+        for (int i = 0; i < array.length; i++) {
+            nums[i] = (int) array[i];
+        }
+        return array.length;
+
+    }
 }
