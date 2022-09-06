ff8ce2a31717e972d8ff08a5d1a9c2a6ffbd73ed
ljl <ljl@qq.com>
2022-09-06 16:38:30 +0800

    add largestNumber

--- src/leetcode/sort/isValid.java
+++ src/leetcode/sort/isValid.java
@@ -120,6 +120,19 @@ public class isValid {
     }
 
     /**
+     * 给定一组非负整数 nums，重新排列每个数的顺序（每个数不可拆分）使之组成一个最大的整数。
+     * @param nums
+     * @return
+     */
+    public String largestNumber(int[] nums) {
+        PriorityQueue<String> heap = new PriorityQueue<>((x, y) -> (y + x).compareTo(x + y));
+        for(int x: nums) heap.offer(String.valueOf(x));
+        String res = "";
+        while(heap.size() > 0) res += heap.poll();
+        if(res.charAt(0) == '0') return "0";
+        return res;
+    }
+    /**
      * 给你一个包含 n 个整数nums，判断nums中是否存在三个元素 a，b，c ，使得a + b + c = 0 ？请你找出所有和为 0 且不重复的三元组。
      * @param nums
      * @return
