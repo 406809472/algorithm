1bfa48808a797cb77cab1e69360ea00c69b2b44e
ljl <ljl@qq.com>
2022-09-28 16:40:34 +0800

    add new

--- src/leetcode/sort/isValid.java
+++ src/leetcode/sort/isValid.java
@@ -47,6 +47,7 @@ public class isValid {
         return array.length;
 
     }
+
     public int findLucky(int[] arr) {
         int[] count = new int[arr.length];
         for (int i = 0; i < arr.length; i++) {
@@ -65,6 +66,7 @@ public class isValid {
 
     /**
      * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+     *
      * @param nums
      * @param target
      * @return
@@ -99,80 +101,132 @@ public class isValid {
     /**
      * 给你一个整数数组nums 和一个整数k ，判断数组中是否存在两个 不同的索引i和j ，满足 nums[i] == nums[j] 且 abs(i - j) <= k 。
      * 如果存在，返回 true ；否则，返回 false 。
-     *滑动窗口
+     * 滑动窗口
      */
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         HashSet set = new HashSet();
-        for(int i  = 0;i<nums.length;i++){
-            if(i>k){
-                set.remove(nums[i-k-1]);
+        for (int i = 0; i < nums.length; i++) {
+            if (i > k) {
+                set.remove(nums[i - k - 1]);
             }
-            if(!set.add(nums[i])){
+            if (!set.add(nums[i])) {
                 return true;
             }
         }
         return false;
     }
-    @Test
-    public void test() {
-        int[] arr = new int[]{1, 0, 1, 1};
-        System.out.println(containsNearbyDuplicate(arr, 1));
-    }
 
     /**
      * 给定一组非负整数 nums，重新排列每个数的顺序（每个数不可拆分）使之组成一个最大的整数。
+     *
      * @param nums
      * @return
      */
     public String largestNumber(int[] nums) {
         PriorityQueue<String> heap = new PriorityQueue<>((x, y) -> (y + x).compareTo(x + y));
-        for(int x: nums) heap.offer(String.valueOf(x));
+        for (int x : nums) heap.offer(String.valueOf(x));
         String res = "";
-        while(heap.size() > 0) res += heap.poll();
-        if(res.charAt(0) == '0') return "0";
+        while (heap.size() > 0) res += heap.poll();
+        if (res.charAt(0) == '0') return "0";
         return res;
     }
+
     /**
      * 给你一个包含 n 个整数nums，判断nums中是否存在三个元素 a，b，c ，使得a + b + c = 0 ？请你找出所有和为 0 且不重复的三元组。
+     *
      * @param nums
      * @return
      */
-    public List<List<Integer>> threeSum(int[] nums){
+    public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> lists = new ArrayList();
-        if (nums == null || nums.length<3){
+        if (nums == null || nums.length < 3) {
             return lists;
         }
         Arrays.sort(nums);
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i]>0){
+            if (nums[i] > 0) {
                 return lists;
             }
-            if (i>0 &&nums[i] == nums[i-1]){
+            if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
             int cur = nums[i];
-            int left = i+1;
-            int right = nums.length-1;
-            while (left<right){
-                int temp =cur + nums[left] + nums[right];
-                if (temp == 0){
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                int temp = cur + nums[left] + nums[right];
+                if (temp == 0) {
                     List<Integer> list = new ArrayList();
                     list.add(cur);
                     list.add(nums[left]);
                     list.add(nums[right]);
                     lists.add(list);
-                    while (left<right && nums[left] == nums[left+1])left++;
-                    while (left<right && nums[right]==nums[right-1])right--;
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
                     left++;
                     right--;
-                }else if (temp<0){
+                } else if (temp < 0) {
                     left++;
-                }
-                else {
+                } else {
                     right--;
                 }
             }
         }
         return lists;
     }
+
+    /**
+     * 给定一个字符串 s ，找出其中不含有重复字符的最长子串的长度。
+     */
+    public int lengthOfLongestSubstring(String s) {
+        if (s.length() == 0 || s == "") {
+            return 0;
+        }
+        int ans = 0;
+        int left = 0;
+        HashMap<Character, Integer> hashMap = new HashMap();
+        for (int i = 0; i < s.length(); i++) {
+            if (hashMap.containsKey(s.charAt(i))) {
+//                  left = hashMap.get(s.charAt(i)) + 1;
+                left = Math.max(left, hashMap.get(s.charAt(i)) + 1);
+            }
+            hashMap.put(s.charAt(i), i);
+            ans = Math.max(ans, i - left + 1);
+        }
+        return ans;
+    }
+
+    /**
+     * 长度最小的子数组
+     *
+     * @param target
+     * @param nums
+     * @return
+     */
+    public int minSubArrayLen(int target, int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int ans = Integer.MAX_VALUE;
+        int start = 0, end = 0;
+        int sum = 0;
+        while (end < n) {
+            sum += nums[end];
+            while (sum >= target) {
+                ans = Math.min(ans, end - start + 1);
+                sum -= nums[start];
+                start++;
+            }
+            end++;
+        }
+        return ans == Integer.MAX_VALUE ? 0 : ans;
+    }
+
+    @Test
+    public void test() {
+        String s = "cdbca";
+        System.out.println(s.charAt(1));
+    }
+
 }
